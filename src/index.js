8c44c32e6a6e3790f646443c8d59ddf470faaffd
Div685 <divyeshpatel685@gmail.com>
2021-04-27 21:37:17 +0300

    setup Eslint and stylelint linters
    - Added .eslintrc.json file
    - Added .stylelintrc.json file

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1 >BookStore</h1>
+    <h1>BookStore</h1>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
