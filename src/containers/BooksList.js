f6edb2b9bcd7be9c64adaaf95b4af72734200671
Div685 <divyeshpatel685@gmail.com>
2021-04-27 23:04:20 +0300

    implement BookList component

---
+++ src/containers/BooksList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function BooksList() {
+  return (
+    <div className="main-booklists">
+      <table>
+        <tr>
+          <th>Book ID</th>
+          <th>title</th>
+          <th>category</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default BooksList;
