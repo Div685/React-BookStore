7046dbacb481c1ba7ebb1e368f73f5102e01b50b
Div685 <divyeshpatel685@gmail.com>
2021-04-28 11:15:43 +0300

    Add action type for create and remove book
    - defined 2 methods createBookAction and removeBookAction

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// This where you’ll define all the Redux actions.
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+export const createBookAction = (book) => ({
+  type: CREATE_BOOK,
+  book,
+});
+
+export const removeBookAction = (book) => ({
+  type: REMOVE_BOOK,
+  book,
+});
