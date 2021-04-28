2fc33d4c4e21eaddd62be7f7ef66db9db829d683
Div685 <divyeshpatel685@gmail.com>
2021-04-28 18:28:35 +0300

    Add title and category as state
    - Added it inside BooksForm component

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
 function BooksForm() {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('Action');
+
   return (
     <form>
       <input type="text" name="title" />
