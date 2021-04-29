a8067eb88eaeb7b3adf068dafd9d9425bc68d300
Div685 <divyeshpatel685@gmail.com>
2021-04-29 13:11:55 +0300

    Add RemoveBook functionality
    - Added handleRemoveBook method
    - passed handleRemoveBook to the Book Component
    - added mapdispatchtoProps method and pass i to the connect method.

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
+import { removeBookAction } from '../actions/index';
 
-function BooksList({ booksList }) {
+function BooksList({ booksList, removeBookAction }) {
+  const handleRemoveBook = (book) => {
+    removeBookAction(book);
+  };
   return (
     <>
       <table>
@@ -12,11 +16,18 @@ function BooksList({ booksList }) {
             <th>Book ID</th>
             <th>title</th>
             <th>category</th>
+            <th>Remove Book</th>
           </tr>
         </thead>
         <tbody>
           {booksList && booksList.length
-            ? booksList.map((item) => <Book key={item.id} book={item} />)
+            ? booksList.map((item) => (
+              <Book
+                key={item.id}
+                book={item}
+                handleRemoveBook={handleRemoveBook}
+              />
+            ))
             : 'No Books! '}
         </tbody>
       </table>
@@ -26,7 +37,14 @@ function BooksList({ booksList }) {
 
 const mapStateToProps = (state) => ({ booksList: state.booksList });
 
-export default connect(mapStateToProps)(BooksList);
+const mapDispatchToProps = (dispatch) => (
+  {
+    removeBookAction: (book) => {
+      dispatch(removeBookAction(book));
+    },
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
 
 BooksList.propTypes = {
   booksList: PropTypes.arrayOf(PropTypes.shape({
@@ -34,4 +52,5 @@ BooksList.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   })).isRequired,
+  removeBookAction: PropTypes.func.isRequired,
 };
