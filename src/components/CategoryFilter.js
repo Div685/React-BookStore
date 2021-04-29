2a27af0c8368f5a3715a10b123736e5cb6b1f1be
Div685 <divyeshpatel685@gmail.com>
2021-04-29 18:50:09 +0300

    Add All Option to the select box.

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -5,6 +5,7 @@ function CategoryFilter() {
   return (
     <div className="selectBox">
       <select name="filterCategory" id="filterCat">
+        <option value="All">All</option>
         {
         categories.map((item) => (
           <option value={item} key={item}>
