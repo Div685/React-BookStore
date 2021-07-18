1eb2bd30a5ededf3e981b27084361b2aa1a679cb
Div685 <divyeshpatel685@gmail.com>
2021-07-18 21:28:40 +0300

    Add dispacher to the getBooks function
    - to fetch data through redux store

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,8 +1,10 @@
+import { addBookAction } from '../actions';
+import store from '../store';
 import authAxios from './request';
 
 export const getBooks = async () => {
   const response = await authAxios().get('books')
-    .then((response) => response.data).catch((error) => error);
+    .then((response) => store.dispatch(addBookAction(response.data))).catch((error) => error);
   return response;
 };
 
