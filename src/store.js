73da2b47dae3b7dc367b8225edb2e233c81c94c0
Div685 <divyeshpatel685@gmail.com>
2021-07-18 21:27:05 +0300

    update redux-store
    - remove initial store values

--- src/store.js
+++ src/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 
@@ -22,7 +23,7 @@ const initialState = {
   ],
 };
 
-const store = createStore(rootReducer, initialState,
+const store = createStore(rootReducer,
   // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
