f93fdc3008272aabc2468a9a4fd226fca058f439
Div685 <divyeshpatel685@gmail.com>
2021-07-21 19:56:10 +0300

    Add Routes to the App component
    - installed react-router-dom package

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
 
 function App() {
   return (
-    <div>
-      <BooksList />
-      <BooksForm />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <BooksList />
+            <BooksForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
