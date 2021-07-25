7aae0271ee96dc2d349145484d36c864058f01a7
Div685 <divyeshpatel685@gmail.com>
2021-07-25 17:04:48 +0300

    Add handleFilterChange method
    - Added useSelector hook to get data from Redux

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,32 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Book from '../components/Book';
-import { getBooks } from '../api/fetchData';
-import { removeBookAction, filterBookAction } from '../actions/index';
+import { filterBooks, getBooks } from '../api/fetchData';
+import { removeBookAction } from '../actions/index';
 import CategoryFilter from '../components/CategoryFilter';
 import '../style/Book.css';
 
 const BooksList = () => {
+  const [bookList, setBookList] = useState([]);
+
   const booksList = useSelector((state) => state.booksList.books);
+
+  const filterList = useSelector((state) => state.filter.book);
 
   useEffect(() => {
     getBooks();
   }, []);
 
+  useEffect(() => {
+    setBookList(booksList);
+  }, [booksList]);
+
+  useEffect(() => {
+    setBookList(filterList);
+  }, [filterList]);
+
   const handleRemoveBook = (book) => {
     removeBookAction(book);
   };
 
-  const handleFilterChange = (filter) => {
-    filterBookAction(filter);
+  const handleFilterChange = async (filter) => {
+    if (filter === 'All') {
+      await getBooks();
+    } else {
+      await filterBooks(filter);
+    }
   };
 
   return (
     <>
       <CategoryFilter handleFilterChange={handleFilterChange} />
       <div className="book_details">
-        {booksList && booksList.length
-          ? booksList.map((item) => (
+        {bookList && bookList.length
+          ? bookList.map((item) => (
             <Book
               key={item.id}
               book={item}
